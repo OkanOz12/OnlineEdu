9107a9502f87d43acf18ba78dccc27f968135843
Okan Özdağlar <Okan zdalar@DESKTOP-BUUVOVE>
2024-12-18 23:49:42 +0300

    Add project files.

---
+++ OnlineEdu.Entity/Entities/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Entity.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public CourseCategory Category { get; set; } //Category sınıfına erişebilmemiz için yol vermiş oldum.
+
+        public decimal Price { get; set; }
+        public bool IsShown { get; set; } //Anasayfa da gösterilme değeri. True ya da false olarak set ettiğim değeri gösterir admin tarafında.
+    }
+}
---
+++ OnlineEdu.Entity/Entities/CourseCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Entity.Entities
+{
+    public class CourseCategory
+    {
+        public int CourseCategoryId { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public bool IsShown { get; set; }  // statüs değer. True,False değerine göre kategorileri listelemek için
+        public List<Course> Courses { get; set; }
+
+    }
+}
